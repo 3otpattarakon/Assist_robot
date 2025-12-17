cdf7887c11abd1f2f3ba404c1724a0b0363e4aff
3otpattarakon <pattarakon203@gmail.com>
2025-12-17 22:13:46 +0700

    Add files via upload

---
+++ assistrobot/src/main/java/com/robot/assistrobot/AssistrobotApplication.java
@@ -0,0 +1,13 @@
+package com.robot.assistrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistrobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistrobotApplication.class, args);
+	}
+
+}
---
+++ assistrobot/src/test/java/com/robot/assistrobot/AssistrobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robot.assistrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistrobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
